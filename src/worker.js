2767c8514baa99f04100d70968f6108e7cfa2a6f
Simon Bull <simon.bull.84@googlemail.com>
2017-11-23 14:16:30 +0000

    Use config schema helper

--- src/worker.js
+++ src/worker.js
@@ -3,15 +3,8 @@ const logger = require('./infrastructure/logger');
 const Monitor = require('./app/monitor');
 const { getProcessorMappings } = require('./app/processors');
 
-const { jobsSchema } = require('login.dfe.config.schema');
-const validationResult = jobsSchema.validate(config);
-if (!validationResult.isValid) {
-  logger.error('Config is invalid!');
-  validationResult.errors.forEach((item) => {
-    logger.error(JSON.stringify(item));
-  });
-  return process.exit(1);
-}
+const { jobsSchema, validateConfigAndQuitOnError } = require('login.dfe.config.schema');
+validateConfigAndQuitOnError(jobsSchema, config, logger);
 
 logger.info('starting');
 
