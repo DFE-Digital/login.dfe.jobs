0ad32902ff0ea18bc068b568ad9f9b1d32822cab
Simon Bull <simon.bull.84@googlemail.com>
2017-11-23 13:42:35 +0000

    Add schema validation

--- src/worker.js
+++ src/worker.js
@@ -1,7 +1,17 @@
 const config = require('./infrastructure/config');
 const logger = require('./infrastructure/logger');
 const Monitor = require('./app/monitor');
-const {getProcessorMappings} = require('./app/processors');
+const { getProcessorMappings } = require('./app/processors');
+
+const { jobsSchema } = require('login.dfe.config.schema');
+const validationResult = jobsSchema.validate(config);
+if (!validationResult.isValid) {
+  logger.error('Config is invalid!');
+  validationResult.errors.forEach((item) => {
+    logger.error(JSON.stringify(item));
+  });
+  return process.exit(1);
+}
 
 logger.info('starting');
 
