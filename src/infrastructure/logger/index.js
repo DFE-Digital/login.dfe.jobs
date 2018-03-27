90e3325940f870d3153280574034c00f55139d63
Simon Bull <simon.bull.84@googlemail.com>
2018-03-27 16:27:54 +0100

    Add DfE AI Winston transport

--- src/infrastructure/logger/index.js
+++ src/infrastructure/logger/index.js
@@ -3,7 +3,7 @@
 const winston = require('winston');
 const config = require('./../config');
 const appInsights = require('applicationinsights');
-const WinstonApplicationInsights = require('winston-azure-application-insights').AzureApplicationInsightsLogger;
+const AppInsightsTransport = require('login.dfe.winston-appinsights');
 
 const logLevel = (config && config.loggerSettings && config.loggerSettings.logLevel) ? config.loggerSettings.logLevel : 'info';
 
@@ -29,8 +29,13 @@ const loggerConfig = {
 loggerConfig.transports.push(new (winston.transports.Console)({ level: logLevel, colorize: true }));
 
 if (config.hostingEnvironment.applicationInsights) {
-  appInsights.setup(config.hostingEnvironment.applicationInsights).start();
-  loggerConfig.transports.push(new WinstonApplicationInsights({ client: appInsights.defaultClient }));
+  appInsights.setup(config.hostingEnvironment.applicationInsights).setAutoCollectConsole(false, false).start();
+  loggerConfig.transports.push(new AppInsightsTransport({
+    client: appInsights.defaultClient,
+    applicationName: config.loggerSettings.applicationName || 'Directories',
+    type: 'event',
+    treatErrorsAsExceptions: true,
+  }));
 }
 
 
