0b512c237d4fcc2b20e5118cb9787e3223e9562d
Simon Bull <simon.bull.84@googlemail.com>
2019-02-08 09:36:14 +0000

    Fix tests

--- test/appTests/processorsTests/getProcessorMappings.test.js
+++ test/appTests/processorsTests/getProcessorMappings.test.js
@@ -1,5 +1,6 @@
 jest.mock('login.dfe.migration.admin.job');
 jest.mock('login.dfe.notification.jobs');
+jest.mock('login.dfe.service-notifications.jobs');
 
 const { getProcessorMappings } = require('./../../../src/app/processors');
 
@@ -13,6 +14,7 @@ const logger = {
 describe('when getting processor mappings', () => {
   let migrationAdminRegister;
   let notificationsRegister;
+  let serviceNotificationsRegister;
 
   beforeEach(() => {
     migrationAdminRegister = jest.fn().mockReturnValue([
@@ -44,6 +46,21 @@ describe('when getting processor mappings', () => {
     ]);
     const notifications = require('login.dfe.notification.jobs');
     notifications.register = notificationsRegister;
+
+    serviceNotificationsRegister = jest.fn().mockReturnValue([
+      {
+        type: 'servicenotifications1',
+        processor: () => {
+        }
+      },
+      {
+        type: 'servicenotifications2',
+        processor: () => {
+        }
+      },
+    ]);
+    const serviceNotifications = require('login.dfe.service-notifications.jobs');
+    serviceNotifications.register = serviceNotificationsRegister;
   });
 
   it('then it should return an array of processors', async () => {
@@ -78,5 +95,15 @@ describe('when getting processor mappings', () => {
     expect(notificationsRegister.mock.calls[0][1]).toBe(logger);
     expect(actual.find((x) => x.type === 'notifications1')).toBeDefined();
     expect(actual.find((x) => x.type === 'notifications2')).toBeDefined();
+  });
+
+  it('then it should register service notification processors', async () => {
+    const actual = await getProcessorMappings(config, logger);
+
+    expect(notificationsRegister.mock.calls.length).toBe(1);
+    expect(notificationsRegister.mock.calls[0][0]).toBe(config);
+    expect(notificationsRegister.mock.calls[0][1]).toBe(logger);
+    expect(actual.find((x) => x.type === 'servicenotifications1')).toBeDefined();
+    expect(actual.find((x) => x.type === 'servicenotifications2')).toBeDefined();
   });
 });
