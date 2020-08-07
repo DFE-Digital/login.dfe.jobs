a7cb00f6d70192a9677c38bd06f1aa406966cbda
Sumanta Sarkar <sumanta.sr@gmail.com>
2020-08-07 09:51:13 +0100

    added exception handling for slack (#177)
    Co-authored-by: Sumanta <>

--- src/services/slackService.js
+++ src/services/slackService.js
@@ -1,17 +1,23 @@
 const { IncomingWebhook } = require('@slack/webhook');
 const config = require('../infrastructure/config');
+const logger = require('../infrastructure/logger');
 
 class SlackService {
 
     async postMessage(message) {
-        let url = config.notifications.slackWebHookUrl;
-        const webhook = new IncomingWebhook(url);
+        try {
+            let url = config.notifications.slackWebHookUrl;
+            const webhook = new IncomingWebhook(url);
 
-        await (async () => {
-            await webhook.send({
-                text: message
-            });
-        })();
+            await (async () => {
+                await webhook.send({
+                    text: message
+                });
+            })();
+        } catch (ex) {
+            logger.error(`Exception in sending slack message`);
+            logger.error(ex);
+        }
     }
 }
 
