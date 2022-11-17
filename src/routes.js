8ae03dd15d29927f2eea396992a354ba92ac4c09
Univer-lion-lab <rafael.tsiakkouris@methods.co.uk>
2022-11-17 15:42:34 +0000

    fix routes & rename worker.js

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ const monitor = require('./app/monitor');
 const jobs = require('./app/jobManagement');
 
 const registerRoutes = (app) => {
-  app.use('/monitor', monitor());
+  app.use('/jobs/monitor', monitor());
   app.use('/jobs', jobs());
 };
 
