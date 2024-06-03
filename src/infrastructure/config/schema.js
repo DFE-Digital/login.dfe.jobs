970ea667d6d652d4151b8b0d0c73ce4f57cd495a
Richard Ward <richard6.ward@education.gov.uk>
2024-06-03 10:30:07 +0100

    Remove repo `login.dfe.migration.admin.jobs`
    Remove repo `login.dfe.migration.admin.jobs` along with associated code/tests etc. This repo is public archive and read-only, and therefore not required anymore.

--- src/infrastructure/config/schema.js
+++ src/infrastructure/config/schema.js
@@ -7,73 +7,6 @@ const queueStorageSchema = new SimpleSchema({
   connectionString: patterns.redis,
 });
 
-const migrationAdminApiClient = new SimpleSchema({
-  url: patterns.url,
-  auth: Object,
-  'auth.type': {
-    type: String,
-    allowedValues: ['aad', 'secret'],
-  },
-  'auth.jwt': {
-    type: String,
-    optional: true,
-    custom: function () {
-      if (this.siblingField('type').value === 'secret' && !this.isSet) {
-        return SimpleSchema.ErrorTypes.REQUIRED
-      }
-    },
-  },
-  'auth.tenant': {
-    type: String,
-    optional: true,
-    custom: function () {
-      if (this.siblingField('type').value === 'aad' && !this.isSet) {
-        return SimpleSchema.ErrorTypes.REQUIRED
-      }
-    },
-  },
-  'auth.authorityHostUrl': {
-    type: String,
-    regEx: /^http(s{0,1}):\/\/.*$/,
-    optional: true,
-    custom: function () {
-      if (this.siblingField('type').value === 'aad' && !this.isSet) {
-        return SimpleSchema.ErrorTypes.REQUIRED
-      }
-    },
-  },
-  'auth.clientId': {
-    type: String,
-    optional: true,
-    custom: function () {
-      if (this.siblingField('type').value === 'aad' && !this.isSet) {
-        return SimpleSchema.ErrorTypes.REQUIRED
-      }
-    },
-  },
-  'auth.clientSecret': {
-    type: String,
-    optional: true,
-    custom: function () {
-      if (this.siblingField('type').value === 'aad' && !this.isSet) {
-        return SimpleSchema.ErrorTypes.REQUIRED
-      }
-    },
-  },
-  'auth.resource': {
-    type: String,
-    optional: true,
-    custom: function () {
-      if (this.siblingField('type').value === 'aad' && !this.isSet) {
-        return SimpleSchema.ErrorTypes.REQUIRED
-      }
-    },
-  },
-});
-const migrationAdminSchema = new SimpleSchema({
-  directories: migrationAdminApiClient,
-  organisations: migrationAdminApiClient,
-});
 
 const adapterSchema = new SimpleSchema({
   type: {
@@ -142,7 +75,6 @@ const schema = new SimpleSchema({
   auth: schemas.apiServerAuth,
   queueStorage: queueStorageSchema,
   persistentStorage: schemas.sequelizeConnection,
-  migrationAdmin: migrationAdminSchema,
   notifications: notificationsSchema,
   publicApi: publicApiSchema,
   serviceNotifications: serviceNotificationsSchema,
