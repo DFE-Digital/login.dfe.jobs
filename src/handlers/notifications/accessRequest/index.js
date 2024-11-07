372ceccc16760e28dbe15106054ee0fa0ef1cefd
Julio Velezmoro <julio28ve@gmail.com>
2024-11-07 16:51:18 +0000

    remove user access request email

--- src/handlers/notifications/accessRequest/index.js
+++ src/handlers/notifications/accessRequest/index.js
@@ -1,14 +1,12 @@
 const accessRequestV1 = require('./accessRequestV1');
 const approverAccessRequestV1 = require('./approverAccessRequestV1');
 const organisationRequestHandlerV1 = require('./organisationRequestHanderV1');
-const userAccessRequestV1 = require('./userAccessRequestV1');
 
 const register = (config, logger) => {
   return [
     accessRequestV1.getHandler(config, logger),
     approverAccessRequestV1.getHandler(config, logger),
     organisationRequestHandlerV1.getHandler(config, logger),
-    userAccessRequestV1.getHandler(config, logger),
   ];
 };
 
