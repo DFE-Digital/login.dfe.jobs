ad6dd55412dc33073e7259a403cf139c7bb31148
Julio Velezmoro <julio28ve@gmail.com>
2024-11-08 10:08:18 +0000

    remove registration complete

--- src/handlers/notifications/registration/index.js
+++ src/handlers/notifications/registration/index.js
@@ -1,11 +1,8 @@
 const registerExistingUserV1 = require('./registerExistingUserV1');
-const registrationCompleteV1 = require('./registrationCompleteV1');
 
 const register = (config, logger) => {
   return [
     registerExistingUserV1.getHandler(config, logger),
-
-    registrationCompleteV1.getHandler(config, logger),
   ];
 };
 
