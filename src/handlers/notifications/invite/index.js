abbbf9de6e545fd923301699a397434168c6b251
Lea Hayes <lea.hayes@education.gov.uk>
2024-11-11 17:02:10 +0000

    Remove unused new user invitation handler.

--- src/handlers/notifications/invite/index.js
+++ src/handlers/notifications/invite/index.js
@@ -1,9 +1,7 @@
-const newUserInvitationV1 = require('./newUserInvitationV1');
 const newUserInvitationV2 = require('./newUserInvitationV2');
 
 const register = (config, logger) => {
   return [
-    newUserInvitationV1.getHandler(config, logger),
     newUserInvitationV2.getHandler(config, logger),
   ];
 };
