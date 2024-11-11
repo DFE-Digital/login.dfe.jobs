57b426299f34f657b65dd1f9260504b4fe15f305
Julio Velezmoro <julio28ve@gmail.com>
2024-11-11 18:45:52 +0000

    remove unused handlers

--- src/handlers/notifications/index.js
+++ src/handlers/notifications/index.js
@@ -2,10 +2,8 @@ const invite = require('./invite');
 const passwordReset = require('./passwordReset');
 const support = require('./support');
 const registration = require('./registration');
-const confirmMigratedEmail = require('./confirmMigratedEmail');
 const changeProfile = require('./changeProfile');
 const accessRequest = require('./accessRequest');
-const unmigratedSaUser = require('./unmigratedSaUser');
 const newServiceAdded = require('./serviceAdded');
 const userOrganisation = require('./userOrganisation');
 const changeUserPermission = require('./userPermissions');
@@ -13,35 +11,31 @@ const serviceRemoved = require('./serviceRemoved');
 const serviceRequested = require('./serviceRequested');
 const serviceRequestRejected = require('./serviceRequestRejected');
 const subServiceRequested = require('./subServiceRequested');
-const subServiceRequestActioned = require('./subServiceRequestActioned')
+const subServiceRequestActioned = require('./subServiceRequestActioned');
 
 const register = (config, logger) => {
   const inviteHandlers = invite.register(config, logger);
   const passwordResetHandlers = passwordReset.register(config, logger);
   const supportHandlers = support.register(config, logger);
   const registrationHandlers = registration.register(config, logger);
-  const confirmMigratedEmailHandlers = confirmMigratedEmail.register(config,logger);
   const changeProfileHandlers = changeProfile.register(config, logger);
   const accessRequestHandler = accessRequest.register(config, logger);
-  const unmigratedSaUserHandler = unmigratedSaUser.register(config, logger);
   const newServiceAddedHandler = newServiceAdded.register(config, logger);
-  const userOrganisationHandler = userOrganisation.register(config,logger);
+  const userOrganisationHandler = userOrganisation.register(config, logger);
   const changeUserPermissionHandler = changeUserPermission.register(config, logger);
   const serviceRemovedHandler = serviceRemoved.register(config, logger);
   const serviceRequestedHandler = serviceRequested.register(config, logger);
   const serviceRequestRejectedHandler = serviceRequestRejected.register(config, logger);
   const subServiceRequestedHandler = subServiceRequested.register(config, logger);
-  const subServiceRequestActionedHandler = subServiceRequestActioned.register(config,logger);
+  const subServiceRequestActionedHandler = subServiceRequestActioned.register(config, logger);
 
   return [
     ...inviteHandlers,
     ...passwordResetHandlers,
     ...supportHandlers,
     ...registrationHandlers,
-    ...confirmMigratedEmailHandlers,
     ...changeProfileHandlers,
     ...accessRequestHandler,
-    ...unmigratedSaUserHandler,
     ...newServiceAddedHandler,
     ...userOrganisationHandler,
     ...changeUserPermissionHandler,
@@ -54,5 +48,5 @@ const register = (config, logger) => {
 };
 
 module.exports = {
-  register
+  register,
 };
