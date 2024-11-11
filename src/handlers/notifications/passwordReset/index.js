eaedb2c70f4ec26647feafa4210f079b04754333
Julio Velezmoro <julio28ve@gmail.com>
2024-11-11 08:29:28 +0000

    remove sa reset password

--- src/handlers/notifications/passwordReset/index.js
+++ src/handlers/notifications/passwordReset/index.js
@@ -1,10 +1,8 @@
 const v1 = require('./passwordResetV1');
-const SAResetv1 = require('./SAResetV1');
 
 const register = (config, logger) => {
   return [
     v1.getHandler(config, logger),
-    SAResetv1.getHandler(config, logger),
   ];
 };
 
