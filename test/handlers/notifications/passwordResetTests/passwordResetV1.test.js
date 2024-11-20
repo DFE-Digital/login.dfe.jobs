193aa5b96c7bbec1f0853a454f0bde1006dabc15
Lea Hayes <lea.hayes@education.gov.uk>
2024-11-20 15:43:05 +0000

    Migrate 'password-reset' template to GOV Notify.

--- test/handlers/notifications/passwordResetTests/passwordResetV1.test.js
+++ test/handlers/notifications/passwordResetTests/passwordResetV1.test.js
@@ -1,23 +1,20 @@
-jest.mock('../../../../src/infrastructure/email');
+jest.mock('../../../../src/infrastructure/notify');
 jest.mock('uuid', () => {
   return {
-    v4: jest.fn().mockReturnValue('some-uuid')
+    v4: jest.fn().mockReturnValue('750f050d-6861-419b-b089-1b78c4db2611')
   }
 });
 
+const { getNotifyAdapter } = require('../../../../src/infrastructure/notify');
+const { getHandler } = require('../../../../src/handlers/notifications/passwordReset/passwordResetV1.js');
+
 const config = {
   notifications: {
-    type: 'disk',
-    migrationUrl: 'http://unit.test/migration/',
     interactionsUrl: 'https://interactions.test',
     helpUrl: 'https://help.url',
   },
 };
-const logger = {
-  info: jest.fn(),
-  error: jest.fn(),
-};
-const data = {
+const jobData = {
   email: 'user.one@unit.test',
   firstName: 'Jane',
   lastName: 'Doe',
@@ -27,87 +24,77 @@ const data = {
 };
 
 describe('when processing a passwordreset_v1 job', () => {
-  let emailSend;
-  let email;
-  let handler;
+  const mockSendEmail = jest.fn();
 
   beforeEach(() => {
-    emailSend = jest.fn();
-    email = require('../../../../src/infrastructure/email');
-    email.getEmailAdapter = jest.fn().mockImplementation(() => {
-      return {
-        send: emailSend,
-      };
-    });
+    mockSendEmail.mockReset();
 
-    handler = require('../../../../src/handlers/notifications/passwordReset/passwordResetV1').getHandler(config, logger);
+    getNotifyAdapter.mockReset();
+    getNotifyAdapter.mockReturnValue({ sendEmail: mockSendEmail });
   });
 
-  it('then it should get email adapter with config and logger', async () => {
-    await handler.processor(data);
+  it('should return a handler with a processor', () => {
+    const handler = getHandler(config);
 
-    expect(email.getEmailAdapter.mock.calls.length).toBe(1);
-    expect(email.getEmailAdapter.mock.calls[0][0]).toBe(config);
-    expect(email.getEmailAdapter.mock.calls[0][1]).toBe(logger);
+    expect(handler).not.toBeNull();
+    expect(handler.type).toBe('passwordreset_v1');
+    expect(handler.processor).not.toBeNull();
+    expect(handler.processor).toBeInstanceOf(Function);
   });
 
-  it('then it should send an email using the password-reset template', async () => {
-    await handler.processor(data);
+  it('should get email adapter with supplied config', async () => {
+    const handler = getHandler(config);
 
-    expect(emailSend.mock.calls.length).toBe(1);
-    expect(emailSend.mock.calls[0][1]).toBe('password-reset');
-    expect(emailSend.mock.calls[0][2]).toEqual({
-      clientId: data.clientId,
-      code: data.code,
-      firstName: data.firstName,
-      lastName: data.lastName,
-      helpUrl: `${config.notifications.helpUrl}/contact-us`,
-      returnUrl: `${config.notifications.interactionsUrl}/some-uuid/resetpassword/${data.uid}/confirm?clientid=${data.clientId}`,
-    });
+    await handler.processor(jobData);
+
+    expect(getNotifyAdapter).toHaveBeenCalledTimes(1);
+    expect(getNotifyAdapter).toHaveBeenCalledWith(config);
   });
 
-  it('then it should send an email to the user', async () => {
-    await handler.processor(data);
+  it('should send email with expected template', async () => {
+    const handler = getHandler(config);
 
-    expect(emailSend.mock.calls[0][0]).toBe(data.email);
+    await handler.processor(jobData);
+
+    expect(mockSendEmail).toHaveBeenCalledTimes(1);
+    expect(mockSendEmail).toHaveBeenCalledWith(
+      'verifyPasswordResetRequest',
+      expect.anything(),
+      expect.anything(),
+    );
   });
 
-  it('then it should include the user first name and last name in the email data', async () => {
-    await handler.processor(data);
+  it('should send email to users email address', async () => {
+    const handler = getHandler(config);
 
-    expect(emailSend.mock.calls[0][2]).toMatchObject({
-      firstName: data.firstName,
-      lastName: data.lastName,
-    });
+    await handler.processor(jobData);
+
+    expect(mockSendEmail).toHaveBeenCalledTimes(1);
+    expect(mockSendEmail).toHaveBeenCalledWith(
+      expect.anything(),
+      jobData.email,
+      expect.anything(),
+    );
   });
 
-  it('then it should include the reset code in the email data', async () => {
-    await handler.processor(data);
+  it('should send email with expected personalisation data', async () => {
+    const handler = getHandler(config);
 
-    expect(emailSend.mock.calls[0][2]).toMatchObject({
-      code: data.code,
-    });
-  });
+    await handler.processor(jobData);
 
-  it('then it should include the client id in the email data', async () => {
-    await handler.processor(data);
-
-    expect(emailSend.mock.calls[0][2]).toMatchObject({
-      clientId: data.clientId,
-    });
-  });
-
-  it('then it should include a link to interactions in the email data', async () => {
-    await handler.processor(data);
-
-    expect(emailSend.mock.calls[0][2]).toMatchObject({
-      returnUrl: `${config.notifications.interactionsUrl}/some-uuid/resetpassword/${data.uid}/confirm?clientid=${data.clientId}`,
-    });
-  });
-
-  it('then it should include a subject', async () =>{
-    await handler.processor(data);
-
-    expect(emailSend.mock.calls[0][3]).toBe('DfE Sign-in: reset your password');
+    expect(mockSendEmail).toHaveBeenCalledTimes(1);
+    expect(mockSendEmail).toHaveBeenCalledWith(
+      expect.anything(),
+      expect.anything(),
+      expect.objectContaining({
+        personalisation: expect.objectContaining({
+          firstName: jobData.firstName,
+          lastName: jobData.lastName,
+          code: jobData.code,
+          returnUrl: 'https://interactions.test/750f050d-6861-419b-b089-1b78c4db2611/resetpassword/65432RFV/confirm?clientid=CLIENT1',
+          helpUrl: 'https://help.url/contact-us',
+        }),
+      }),
+    );
   });
 });
