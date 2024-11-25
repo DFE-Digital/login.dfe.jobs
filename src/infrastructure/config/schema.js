09af33d5b6ec5079dc703cc9fb558d4d11db6c93
Lea Hayes <lea.hayes@education.gov.uk>
2024-11-25 11:39:30 +0000

    Add all expected GOV Notify template mappings to config schema.

--- src/infrastructure/config/schema.js
+++ src/infrastructure/config/schema.js
@@ -24,10 +24,38 @@ const adapterSchema = new SimpleSchema({
 });
 
 const govNotifyEmailTemplatesSchema = new SimpleSchema({
+  // Access requests
+  approverRequestAccess: String,
+  userAddedToOrganisation: String,
+  userPermissionLevelChanged: String,
+  userRemovedFromOrganisation: String,
+  userRequestForOrganisationAccessApproved: String,
+  userRequestForOrganisationAccessRejected: String,
+  userRequestForServiceApprovedV1: String,
+  userRequestForServiceApprovedV2: String,
+  userRequestForServiceRejected: String,
+  userRequestsForSubServicesApproved: String,
+  userRequestsForSubServicesRejected: String,
+  userRequestToApproverForServiceAccess: String,
+  userRequestToApproverForSubServiceAccess: String,
+  userServiceRemoved: String,
+
+  // Entra migration
+
+  // Profile
+  notifyChangeEmailAddress: String,
+  verifyChangeEmailAddress: String,
+  verifyPasswordResetRequest: String,
+
+  // Support
+  supportRequest: String,
+  supportRequestOverdue: String,
+
+  // User invitations
   inviteNewUser: String,
   inviteNewUserEntra: String,
+  notifyExistingUserWhenAttemptingToRegisterAgain: String,
   selfRegisterNewAccount: String,
-  verifyChangeEmailAddress: String,
 });
 
 const govNotifyTemplatesSchema = new SimpleSchema({
