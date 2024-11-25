4e034aa877cdb0045de3eb6aff9569660db0a98f
Richard Ward <richard.ward@methods.co.uk>
2024-11-25 12:14:52 +0000

    Add MonitorBull for BullMQ

---
+++ src/infrastructure/jobQueue/MonitorBull.js
@@ -0,0 +1,54 @@
+const { Worker } = require('bullmq');
+const config = require('../config');
+const logger = require('../logger');
+
+const process = async (job, processor, done) => {
+  try {
+    logger.info(`MonitorBull: received job ${job.id} of type ${job.name}`);
+    processor(job.data);
+    logger.info(`MonitorBull: processed job ${job.id}`);
+    done();
+  } catch (err) {
+    logger.error(`Error processing job ${job.id}`, { job: { name: job.name, id: job.id }, error: { message: err.message, stack: err.stack } });
+    done(err);
+  }
+};
+
+class MonitorBull {
+  constructor(processorMapping) {
+    this.processorMapping = processorMapping;
+    this.workers = [];
+    this.status = 'stopped';
+  }
+
+  get currentStatus() {
+    return this.status;
+  }
+
+  start() {
+    const connectionString = (config.queueStorage && config.queueStorage.connectionString) ? config.queueStorage.connectionString : 'redis://127.0.0.1:6379';
+
+    // Closely matches Kue which "creates" a worker per "mapping.type"
+    this.processorMapping.forEach((mapping) => {
+      logger.debug(`MonitorBull: start monitoring ${mapping.type}`);
+      const worker = new Worker(mapping.type, async (job) => { process(job, mapping.processor); }, { connection: { url: connectionString } });
+      this.workers.push(worker);
+    });
+
+    this.status = 'started';
+  }
+
+  // Stop all registered workers https://docs.bullmq.io/guide/workers/graceful-shutdown
+  async stop() {
+    try {
+      const stoppers = this.workers.map((worker) => worker.close());
+      await Promise.all(stoppers);
+      this.status = 'stopped';
+      logger.info('MonitorBull: stopped');
+    } catch (err) {
+      logger.error('MonitorBull: failed stop signal', { error: { message: err.message, stack: err.stack } });
+    }
+  }
+}
+
+module.exports = MonitorBull;
