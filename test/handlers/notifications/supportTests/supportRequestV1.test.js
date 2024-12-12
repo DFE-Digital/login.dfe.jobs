7ddf6ec564e90885d94b7c1c542e764541fdbaaa
Richard Ward <richard.ward@methods.co.uk>
2024-12-12 15:15:44 +0000

    Migrate `supportrequest_v1` template to GOV Notify.

--- test/handlers/notifications/supportTests/supportRequestV1.test.js
+++ test/handlers/notifications/supportTests/supportRequestV1.test.js
@@ -1,36 +1,37 @@
-jest.mock('../../../../src/infrastructure/email');
-
-const { getEmailAdapter } = require('../../../../src/infrastructure/email');
+jest.mock('../../../../src/infrastructure/notify');
+const { getNotifyAdapter } = require('../../../../src/infrastructure/notify');
 const { getHandler } = require('../../../../src/handlers/notifications/support/supportRequestV1');
-const emailSend = jest.fn();
 
 const config = {
   notifications: {
-    supportEmailAddress: 'support@unit.tests',
+    helpUrl: 'https://help.dfe.signin',
+    supportEmailAddress: 'mock-support-email-address',
   },
 };
-const logger = {};
+
 const jobData = {
-  name: 'User One',
-  email: 'user.one@unit.tests',
-  orgName: 'org1',
-  urn: '1234',
-  message: 'I am having issues signing in using my new details',
-  service: 'Service Name',
-  type: 'other',
-  typeAdditionalInfo: 'type of issue',
+  name: 'mock-name',
+  email: 'mock-email',
+  orgName: 'mock-org',
+  urn: 'mock-urn',
+  service: 'mock-service',
+  type: 'mock-type',
+  message: 'mock-message',
+  typeAdditionalInfo: 'mock-type-additional-info',
 };
 
 describe('When handling supportrequest_v1 job', () => {
-  beforeEach(() => {
-    emailSend.mockReset();
+  const mockSendEmail = jest.fn();
 
-    getEmailAdapter.mockReset();
-    getEmailAdapter.mockReturnValue({ send: emailSend });
+  beforeEach(() => {
+    mockSendEmail.mockReset();
+
+    getNotifyAdapter.mockReset();
+    getNotifyAdapter.mockReturnValue({ sendEmail: mockSendEmail });
   });
 
-  it('then it should return a handler with a processor', () => {
-    const handler = getHandler(config, logger);
+  it('should return a handler with a processor', async () => {
+    const handler = getHandler(config);
 
     expect(handler).not.toBeNull();
     expect(handler.type).toBe('supportrequest_v1');
@@ -38,58 +39,122 @@ describe('When handling supportrequest_v1 job', () => {
     expect(handler.processor).toBeInstanceOf(Function);
   });
 
-  it('then it should get email adapter with supplied config and logger', async () => {
-    const handler = getHandler(config, logger);
+  it('should get email adapter with supplied config', async () => {
+    const handler = getHandler(config);
 
     await handler.processor(jobData);
 
-    expect(getEmailAdapter.mock.calls).toHaveLength(1);
-    expect(getEmailAdapter.mock.calls[0][0]).toBe(config);
-    expect(getEmailAdapter.mock.calls[0][1]).toBe(logger);
+    expect(getNotifyAdapter).toHaveBeenCalledTimes(1);
+    expect(getNotifyAdapter).toHaveBeenCalledWith(config);
   });
 
-  it('then it should send email to support email address', async () => {
-    const handler = getHandler(config, logger);
+  it('should send email with expected template', async () => {
+    const handler = getHandler(config);
 
     await handler.processor(jobData);
 
-    expect(emailSend.mock.calls).toHaveLength(1);
-    expect(emailSend.mock.calls[0][0]).toBe(config.notifications.supportEmailAddress);
+    expect(mockSendEmail).toHaveBeenCalledTimes(1);
+    expect(mockSendEmail).toHaveBeenCalledWith(
+      'supportRequest',
+      expect.anything(),
+      expect.anything(),
+    );
   });
 
-  it('then it should send email using support-request template', async () => {
-    const handler = getHandler(config, logger);
+  it('should send email to users email address', async () => {
+    const handler = getHandler(config);
 
     await handler.processor(jobData);
 
-    expect(emailSend.mock.calls).toHaveLength(1);
-    expect(emailSend.mock.calls[0][1]).toBe('support-request');
+    expect(mockSendEmail).toHaveBeenCalledTimes(1);
+    expect(mockSendEmail).toHaveBeenCalledWith(
+      expect.anything(),
+      config.notifications.supportEmailAddress,
+      expect.anything(),
+    );
   });
 
-  it('then it should send email using request data as model', async () => {
-    const handler = getHandler(config, logger);
+  it.each([
+    ['', '', false], [null, '', false], ['   ', '', false], [undefined, '', false], ['mock-some-additional-info', 'mock-some-additional-info', true],
+  ])('should send an email with expected personalisation data, with the typeAdditionalInfo set as %s, the expected typeAdditionalInfo should be %s and the header should be visible %s', async (typeAdditionalInfo, expectedTypeAdditionalInfo, expectedShowAdditionalInfoHeader) => {
+    const handler = getHandler(config);
+    const data = { ...jobData, ...{ typeAdditionalInfo } };
+    await handler.processor(data);
 
-    await handler.processor(jobData);
-
-    expect(emailSend.mock.calls).toHaveLength(1);
-    expect(emailSend.mock.calls[0][2]).toEqual({
-      name: jobData.name,
-      email: jobData.email,
-      orgName: jobData.orgName,
-      urn: jobData.urn,
-      message: jobData.message,
-      service: jobData.service,
-      type: jobData.type,
-      typeAdditionalInfo: jobData.typeAdditionalInfo,
-    });
+    expect(mockSendEmail).toHaveBeenCalledTimes(1);
+    expect(mockSendEmail).toHaveBeenCalledWith(
+      expect.anything(),
+      expect.anything(),
+      expect.objectContaining({
+        personalisation: expect.objectContaining({
+          name: data.name,
+          email: data.email,
+          orgName: data.orgName,
+          urn: data.urn,
+          service: data.service,
+          type: data.type,
+          message: data.message,
+          showAdditionalInfoHeader: expectedShowAdditionalInfoHeader,
+          typeAdditionalInfo: expectedTypeAdditionalInfo,
+          helpUrl: `${config.notifications.helpUrl}/contact-us`,
+        }),
+      }),
+    );
   });
 
-  it('then it should send email with subject', async () => {
-    const handler = getHandler(config, logger);
+  it.each([
+    ['', ''], [null, ''], [undefined, ''], ['mock-some-urn', 'mock-some-urn'],
+  ])('should send an email with expected personalisation data, with the urn set as %s, the expected urn should be %s', async (urn, expectedUrn) => {
+    const handler = getHandler(config);
+    const data = { ...jobData, ...{ urn } };
+    await handler.processor(data);
 
-    await handler.processor(jobData);
+    expect(mockSendEmail).toHaveBeenCalledTimes(1);
+    expect(mockSendEmail).toHaveBeenCalledWith(
+      expect.anything(),
+      expect.anything(),
+      expect.objectContaining({
+        personalisation: expect.objectContaining({
+          name: data.name,
+          email: data.email,
+          orgName: data.orgName,
+          urn: expectedUrn,
+          service: data.service,
+          type: data.type,
+          message: data.message,
+          showAdditionalInfoHeader: true,
+          typeAdditionalInfo: data.typeAdditionalInfo,
+          helpUrl: `${config.notifications.helpUrl}/contact-us`,
+        }),
+      }),
+    );
+  });
 
-    expect(emailSend.mock.calls).toHaveLength(1);
-    expect(emailSend.mock.calls[0][3]).toBe('DfE Sign-in service desk request');
+  it.each([
+    ['', ''], [null, ''], [undefined, ''], ['mock-a-orgName', 'mock-a-orgName'],
+  ])('should send an email with expected personalisation data, with the orgName set as %s, the expected orgName should be %s', async (orgName, expectedOrgName) => {
+    const handler = getHandler(config);
+    const data = { ...jobData, ...{ orgName } };
+    await handler.processor(data);
+
+    expect(mockSendEmail).toHaveBeenCalledTimes(1);
+    expect(mockSendEmail).toHaveBeenCalledWith(
+      expect.anything(),
+      expect.anything(),
+      expect.objectContaining({
+        personalisation: expect.objectContaining({
+          name: data.name,
+          email: data.email,
+          orgName: expectedOrgName,
+          urn: data.urn,
+          service: data.service,
+          type: data.type,
+          message: data.message,
+          showAdditionalInfoHeader: true,
+          typeAdditionalInfo: data.typeAdditionalInfo,
+          helpUrl: `${config.notifications.helpUrl}/contact-us`,
+        }),
+      }),
+    );
   });
 });
