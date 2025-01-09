bfd06ff09cc6018b356289a9df9f72e5154ed449
Julio Velezmoro <julio28ve@gmail.com>
2025-01-09 15:31:40 +0000

    Migrate `userremovedfromorganisationrequest_v1` template to GOV Notify.

--- src/handlers/notifications/userOrganisation/removedUserFromOrgV1.js
+++ src/handlers/notifications/userOrganisation/removedUserFromOrgV1.js
@@ -1,24 +1,25 @@
-const { getEmailAdapter } = require('../../../infrastructure/email');
+const { getNotifyAdapter } = require('../../../infrastructure/notify');
 
 const process = async (config, logger, data) => {
-  const email = getEmailAdapter(config, logger);
-  await email.send(data.email, 'user-removed-from-organisation', {
-    firstName: data.firstName,
-    lastName: data.lastName,
-    orgName: data.orgName,
-    signInUrl: `${config.notifications.servicesUrl}`,
-    helpUrl: `${config.notifications.helpUrl}/contact`,
-  }, `DfE Sign-in - Your access to ${data.orgName} has been revoked`)
+  const notify = getNotifyAdapter(config);
+
+  await notify.sendEmail('userRemovedFromOrganisation', data.email, {
+    personalisation: {
+      firstName: data.firstName,
+      lastName: data.lastName,
+      orgName: data.orgName,
+      signInUrl: `${config.notifications.servicesUrl}`,
+      helpUrl: `${config.notifications.helpUrl}/contact`,
+    },
+  });
 };
 
-const getHandler = (config, logger) => {
-  return {
-    type: 'userremovedfromorganisationrequest_v1',
-    processor: async (data) => {
-      await process(config, logger, data);
-    }
-  }
-};
+const getHandler = (config, logger) => ({
+  type: 'userremovedfromorganisationrequest_v1',
+  processor: async (data) => {
+    await process(config, logger, data);
+  },
+});
 
 module.exports = {
   getHandler,
