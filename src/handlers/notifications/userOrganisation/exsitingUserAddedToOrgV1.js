39e7bc9539a23fbc6c315e2ecaeadfd960a53c19
Julio Velezmoro <julio28ve@gmail.com>
2025-01-09 15:28:16 +0000

    Migrate `useraddedtoorganisationrequest_v1` template to GOV Notify.

--- src/handlers/notifications/userOrganisation/exsitingUserAddedToOrgV1.js
+++ src/handlers/notifications/userOrganisation/exsitingUserAddedToOrgV1.js
@@ -1,24 +1,25 @@
-const { getEmailAdapter } = require('../../../infrastructure/email');
+const { getNotifyAdapter } = require('../../../infrastructure/notify');
 
 const process = async (config, logger, data) => {
-  const email = getEmailAdapter(config, logger);
-  await email.send(data.email, 'user-added-to-organisation', {
-    firstName: data.firstName,
-    lastName: data.lastName,
-    orgName: data.orgName,
-    signInUrl: `${config.notifications.servicesUrl}`,
-    helpUrl: `${config.notifications.helpUrl}/contact`,
-  }, `DfE Sign-in - You can now access ${data.orgName} through your DfE Sign-in account`)
+  const notify = getNotifyAdapter(config);
+
+  await notify.sendEmail('userAddedToOrganisation', data.email, {
+    personalisation: {
+      firstName: data.firstName,
+      lastName: data.lastName,
+      orgName: data.orgName,
+      signInUrl: `${config.notifications.servicesUrl}`,
+      helpUrl: `${config.notifications.helpUrl}/contact`,
+    },
+  });
 };
 
-const getHandler = (config, logger) => {
-  return {
-    type: 'useraddedtoorganisationrequest_v1',
-    processor: async (data) => {
-      await process(config, logger, data);
-    }
-  }
-};
+const getHandler = (config, logger) => ({
+  type: 'useraddedtoorganisationrequest_v1',
+  processor: async (data) => {
+    await process(config, logger, data);
+  },
+});
 
 module.exports = {
   getHandler,
